a3ac90ce452f807457a7011148841f67cd7603a3
DanielaSterian <62256164+DanielaSterian@users.noreply.github.com>
2022-08-20 20:15:06 +0300

    Styling

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('security', './assets/login.js')
+    .addEntry('login', './assets/login.js')
+    .addEntry('register', './assets/register.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
